2b7b58e73f308e17afddf73911b2b5cbb6baf54d
Minh Huynh <mhuynh09@gmail.com>
2018-03-16 13:29:43 +1100

    Added config.json

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -3,4 +3,12 @@ import ReactDOM from 'react-dom';
 import Messager from './components/Messager.jsx';
 import '../css/style.css';
 
-ReactDOM.render(<Messager url="ws://localhost:1001"/>, document.getElementById('app'));
+const CONFIG_URL = 'config.json';
+
+fetch(CONFIG_URL)
+.then((response) => response.json())
+.then((json) => {
+    ReactDOM.render(
+        <Messager url={json.URL} />, document.getElementById('app')
+    );  
+});
