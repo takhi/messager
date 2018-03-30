8acb17dcdd1c411d629dfa8f294751f1ad3397f7
Minh Huynh <mhuynh09@gmail.com>
2018-03-30 11:39:40 +1100

    Colored at me messages

--- src/js/components/Message.jsx
+++ src/js/components/Message.jsx
@@ -1,14 +1,23 @@
 import React, {Component} from 'react';
 
 export default class Message extends Component {
+    constructor(props) {
+        super(props);
+        this.atMe = new RegExp(`@${props.me}`);
+    }
     render() {
+        let messageStyle = 'message';
+        if (this.props.me !== this.props.user) {
+            messageStyle += ' friend';
+            if (this.props.text.search(this.atMe) >= 0) messageStyle += ' atMe';
+        }
         return (
             <div className="message-container">
-                <div className={`message${this.props.me === this.props.user ? '' : ' friend'}`}>
+                <div className={messageStyle}>
                     <div className="user">{this.props.user}</div>
                     <div className="text">{this.props.text}</div>
                 </div>
             </div>
-        );
+        )
     }
 };
